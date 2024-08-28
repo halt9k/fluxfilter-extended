affe20993a5afa6aff55c3b58d2df2dd29d4c1d5
halt9k <49090368+halt9k@users.noreply.github.com>
2024-08-29 02:01:24 +0300

    draft of execution experiment

--- src/reddyproc/web_tool_bridge.r
+++ src/reddyproc/web_tool_bridge.r
@@ -1,20 +1,22 @@
-rm(list = ls())
-ias_output_prefix = 'tv_fy4'
+library(REddyProc)
+
+source("src/reddyproc/eddyproc_web_tool_copy.r", chdir = T)
+source("src/reddyproc/calc_averages.r", chdir = T)
 
 
 eddyproc_config <- list(
     # siteId = 'yourSiteID',
     siteId = ias_output_prefix,
-    
+
     isToApplyUStarFiltering = TRUE,
     # uStarSeasoning = "WithinYear", "Continuous" , ...
-    
+
     # could be more levels somewhere around
     uStarSeasoning =  factor("Continuous", levels = "Continuous"),
     uStarMethod = factor("RTw", levels = "RTw"),
-    
+
     isBootstrapUStar = FALSE,
-    
+
     isToApplyGapFilling = TRUE,
     isToApplyPartitioning = TRUE,
     # "Reichstein05", "Lasslop10", ...
@@ -22,15 +24,15 @@ eddyproc_config <- list(
     latitude = 56.5,
     longitude = 32.6,
     timezone = +3,
-    
+
     # there is also $temperatureVarName ?
     temperatureDataVariable = "Tair",
-    
+
     isCatchingErrorsEnabled = TRUE,
-    
+
     input_format = "onlinetool",
     output_format = "onlinetool",
-    
+
     # figureFormat used from processEddyData
     useDevelopLibraryPath = FALSE,
     debugFlags = ""
@@ -49,12 +51,8 @@ unlink(file.path(OUTPUT_DIR, "*.csv"))
 unlink(file.path(OUTPUT_DIR, "output.txt"))
 
 
-options(max.print = 50)
-# fix of stderr output spammed under rpy2.ipython
-sink(stdout(), type = "message")
+# options(max.print = 50)
 
 ext = tools::file_ext(OUTPUT_PLOTS_MASK)
 df_output <- processEddyData(eddyproc_config, dataFileName = INPUT_FILE, figureFormat = ext)
-
-source("src/reddyproc/calc_averages.r", chdir = T)
 calc_averages(df_output, OUTPUT_DIR, eddyproc_config$siteId)
