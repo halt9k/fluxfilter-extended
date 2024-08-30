cfeb541e0705d1131a09708681cd9198d7d10e7c
halt9k <49090368+halt9k@users.noreply.github.com>
2024-08-30 18:04:13 +0300

    draft of execution experiment

--- src/cells_mirror/cell_eddyproc_process.R
+++ src/cells_mirror/cell_eddyproc_process.R
@@ -1,20 +1,17 @@
-rm(list = ls())
-ias_output_prefix = 'tv_fy4'
-
 
 eddyproc_config <- list(
     # siteId = 'yourSiteID',
     siteId = ias_output_prefix,
-    
+
     isToApplyUStarFiltering = TRUE,
     # uStarSeasoning = "WithinYear", "Continuous" , ...
-    
+
     # could be more levels somewhere around
     uStarSeasoning =  factor("Continuous", levels = "Continuous"),
     uStarMethod = factor("RTw", levels = "RTw"),
-    
+
     isBootstrapUStar = FALSE,
-    
+
     isToApplyGapFilling = TRUE,
     isToApplyPartitioning = TRUE,
     # "Reichstein05", "Lasslop10", ...
@@ -22,15 +19,15 @@ eddyproc_config <- list(
     latitude = 56.5,
     longitude = 32.6,
     timezone = +3,
-    
+
     # there is also $temperatureVarName ?
     temperatureDataVariable = "Tair",
-    
+
     isCatchingErrorsEnabled = TRUE,
-    
+
     input_format = "onlinetool",
     output_format = "onlinetool",
-    
+
     # figureFormat used from processEddyData
     useDevelopLibraryPath = FALSE,
     debugFlags = ""
@@ -49,12 +46,26 @@ unlink(file.path(OUTPUT_DIR, "*.csv"))
 unlink(file.path(OUTPUT_DIR, "output.txt"))
 
 
+# 1.3.2 vs 1.3.3 have different outputs
+# to test,
+# install.packages('https://cran.r-project.org/bin/windows/contrib/4.1/REddyProc_1.3.2.zip', repos = NULL, type = "binary")
+install_if_missing <- function(package, repos) {
+    if (!require(package, character.only = TRUE)) {
+        install.packages(package, dependencies = TRUE, repos=repos)
+        library(package, character.only = TRUE)
+    }
+}
+install_if_missing("REddyProc", repos='http://cran.rstudio.com/')
+
+
+library(REddyProc)
+source("src/reddyproc/eddyproc_web_tool_copy.r", chdir = T)
+source("src/reddyproc/calc_averages.r", chdir = T)
+
 options(max.print = 50)
 # fix of stderr output spammed under rpy2.ipython
 sink(stdout(), type = "message")
 
 ext = tools::file_ext(OUTPUT_PLOTS_MASK)
 df_output <- processEddyData(eddyproc_config, dataFileName = INPUT_FILE, figureFormat = ext)
-
-source("src/reddyproc/calc_averages.r", chdir = T)
 calc_averages(df_output, OUTPUT_DIR, eddyproc_config$siteId)
