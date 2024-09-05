25dab093c27b266804c5e45ebd02a07c4eda1d1f
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-05 10:19:52 +0300

    wip

--- src/reddyproc/postprocess_draw_graphs.py
+++ src/reddyproc/postprocess_draw_graphs.py
@@ -1,5 +1,6 @@
 import io
-from typing import List
+from pathlib import Path
+from typing import List, Union
 
 from IPython.core.display import Markdown
 from IPython.display import display
@@ -10,19 +11,56 @@ import glob
 import src.helpers.os_helpers  # noqa: F401
 from src.helpers.image_tools import crop_borders, split_image_vertical
 
-all_img_paths = []
+
+RAW_EDDY_DIR = 'output/REddyProc'
 
 
-def update_img_list(ls):
-    ls[:] = glob.glob('output/REddyProc/*.png')
+def get_tag_paths(tags: Union[str, List[str]], dir = RAW_EDDY_DIR, ext ='.png'):
+    # tags are same as unique file name endings
+    # file must exist
+
+    tags_arr = [tags] if type(tags) is str else tags
+    all_img_paths = Path(dir).glob('*' + ext)
+
+    result = []
+    for tag in tags_arr:
+        fname_end = tag + ext
+        matches = [path for path in all_img_paths if str(path).endswith(fname_end)]
+        assert matches == 1, f"Expected 1 file: {fname_end} found {matches}"
+        result += (tag, matches[0])
+
+    return result[0] if type(tags) is str else result
 
 
-def get_unique_path(fname_end):
-    fname_end = fname_end + '.'
-    img = [path for path in all_img_paths if fname_end in path]
-    found_matches = len(img)
-    assert found_matches == 1, f"Expected 1 file: {fname_end} found {found_matches}"
-    return img[0]
+def replace_fname_end(fname, tag, new_tag):
+    return fname.replace(tag + '.', new_tag + '.')
+
+
+def extract_legends(img_tags, in_dir, out_dir, legend_postfix):
+
+    pass
+
+
+
+def prepare_images(tags_crop: List[str], crop_postfix,
+                   remove_legends: List[str], removed_legend_postfix):
+
+    tp = get_tag_paths(tags_crop)
+    for tag, path in tp:
+        new_path = replace_fname_end(path, tag, tag + crop_postfix)
+
+        img = Image.open(path)
+        cropped = crop_borders(img)
+        cropped.save(new_path)
+
+    tp = get_tag_paths(remove_legends)
+    for tag, path in tp:
+        new_path = replace_fname_end(path, tag, tag + removed_legend_postfix)
+
+        img = Image.open(path)
+        cropped = crop_borders(img)
+        left, _ = split_image_vertical(cropped)
+        left.save(new_path)
 
 
 def display_image_row(paths):
@@ -36,38 +74,13 @@ def display_image_row(paths):
     display(hbox)
 
 
-# TODO extract to tests to simplify non-Jupiter run?
-def prepare_images(crop: List[str], crop_postfix,  remove_legends: List[str], removed_legend_postfix):
-    update_img_list(all_img_paths)
-    for tag in crop:
-        path = get_unique_path(tag)
-        new_path = path.replace('.png', crop_postfix + '.png')
-
-        img = Image.open(path)
-        cropped = crop_borders(img)
-        cropped.save(new_path)
-
-    update_img_list(all_img_paths)
-
-    for tag in remove_legends:
-        path = get_unique_path(tag)
-        new_path = path.replace('.png', removed_legend_postfix + '.png')
-
-        img = Image.open(path)
-        cropped = crop_borders(img)
-        left, _ = split_image_vertical(cropped)
-        left.save(new_path)
-
-
 def display_images(output_order):
-    update_img_list(all_img_paths)
-
     for output_step in output_order:
         if type(output_step) is str:
             title_text = output_step
             display(Markdown("## " + title_text))
         elif type(output_step) is list:
-            paths = [get_unique_path(tag) for tag in output_step]
+            paths = [get_tag_paths(tag) for tag in output_step]
             display_image_row(paths)
         else:
             raise Exception("Wrong OUTPUT_HEADERS contents")
