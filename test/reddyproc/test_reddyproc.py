3ef21b7eb914aa938a08c2f44cec5bbbdc5cb924
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-06 10:29:06 +0300

    wip: test and draft of image polishing

--- test/reddyproc/test_reddyproc.py
+++ test/reddyproc/test_reddyproc.py
@@ -1,3 +1,4 @@
+import shutil
 from pathlib import Path
 import os, sys
 import pytest
@@ -25,6 +26,7 @@ def test_process(use_r_from_python_env):
 
 
 def test_draw():
+    shutil.copytree('test/reddyproc/test_reddyproc_process/output_sample', 'output/REddyProc', dirs_exist_ok=True)
     import src.cells_mirror.cell_reddyproc_draw  # noqa: F401
 
 
