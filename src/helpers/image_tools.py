dbb6ce63504daf0c07fa9a0fc1bbb3026a5ea451
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-07 02:59:56 +0300

    wip: extract_heatmap_legends works

--- src/helpers/image_tools.py
+++ src/helpers/image_tools.py
@@ -1,27 +1,43 @@
 from enum import Enum, auto
 
 import numpy as np
-from PIL import Image, ImageChops
+from PIL import Image, ImageChops, ImageColor
 
 
 class Direction(Enum):
     VERTICAL, HORIZONTAL = 1, 2
 
 
-def crop_borders(img, margin=10):
+def crop_monocolor_borders(img, sides='LTRB', col=None,  margin=10):
     img_rgb = img.convert("RGB")
-    bg = Image.new("RGB", img_rgb.size, img_rgb.getpixel((0, 0)))
+    w, h = img_rgb.size
+
+    consistent_borders = (img_rgb.getpixel((0, 0)) == img_rgb.getpixel((w-1, h-1)) ==
+                          img_rgb.getpixel((w-1, 0)) == img_rgb.getpixel((0, h-1)))
+
+    if not consistent_borders:
+        print('WARNING: Cannot crop image, border color inconsistent')
+        return img
+
+    if not col:
+        col = img_rgb.getpixel((0, 0))
+
+    bg = Image.new("RGB", img_rgb.size, col)
     diff = ImageChops.difference(img_rgb, bg)
     diff = ImageChops.add(diff, diff, 2.0, -100)
-    bbox = diff.getbbox()
 
-    if bbox:
-        if img.getbbox() != bbox:
-            bbox = (bbox[0] - margin, bbox[1] - margin, bbox[2] + margin, bbox[3] + margin )
-        return img.crop(bbox)
-    else:
-        print('Warning: image borders crop failed')
+    bbox_crop = diff.getbbox()
+    bbox = img_rgb.getbbox()
+
+    if not bbox_crop:
+        print('WARNING: Cannot crop image, border color inconsistent')
         return img
+
+    bbox_crop_mg = (max(bbox_crop[0] - margin, 0),  max(bbox_crop[1] - margin, 0),
+                    min(bbox_crop[2] + margin, bbox[2]),min(bbox_crop[3] + margin, bbox[3]))
+    mask = np.array(['L' in sides, 'T' in sides, 'R' in sides, 'B' in sides]).astype(int)
+    bbox_final = bbox * (1 - mask) + bbox_crop_mg * mask
+    return img.crop(tuple(bbox_final))
 
 
 def split_image(img: Image, direction: Direction, n):
@@ -38,10 +54,31 @@ def split_image(img: Image, direction: Direction, n):
     return imgs
 
 
-def remove_white_strip(img: np.array, direction: Direction, percent_from, percent_to):
-    img = Image.open('image.jpg').convert('RGB')
+def grid_images(images, max_horiz=np.iinfo(int).max):
+    n_images = len(images)
+    n_horiz = min(n_images, max_horiz)
+    h_sizes, v_sizes = [0] * n_horiz, [0] * (n_images // n_horiz)
+    for i, im in enumerate(images):
+        h, v = i % n_horiz, i // n_horiz
+        h_sizes[h] = max(h_sizes[h], im.size[0])
+        v_sizes[v] = max(v_sizes[v], im.size[1])
 
-    crop = img.crop((x1, y1, x2, y2))
-    pixels = np.array(crop)
+    h_sizes, v_sizes = np.cumsum([0] + h_sizes), np.cumsum([0] + v_sizes)
+    im_grid = Image.new('RGB', (h_sizes[-1], v_sizes[-1]), color='white')
+    for i, im in enumerate(images):
+        im_grid.paste(im, (h_sizes[i % n_horiz], v_sizes[i // n_horiz]))
+    return im_grid
 
-    is_white = np.all(pixels == [255, 255, 255])
+
+def remove_strip(img: np.array, strip_axis: Direction, percent_at):
+    w, h = img.size
+    assert 0 <= percent_at <= 1
+
+    if strip_axis == Direction.VERTICAL:
+        imgs = [img.crop((0, 0, w * percent_at, h)),
+                img.crop((w * percent_at, 0, w, h))]
+        return grid_images(imgs, 2)
+    elif strip_axis == Direction.HORIZONTAL:
+        imgs = [img.crop((0, 0, w, h * percent_at)),
+                img.crop((0, h * percent_at, w, h))]
+        return grid_images(imgs, 1)
