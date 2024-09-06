269f80ab34bb35f95082186e0cceb0f4db1c6bfc
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-06 08:26:32 +0300

    wip

--- src/cells_mirror/cell_reddyproc_draw.py
+++ src/cells_mirror/cell_reddyproc_draw.py
@@ -1,3 +1,8 @@
+import src.reddyproc.postprocess_draw_graphs as dg
+dg.BKP_IMG_DIR = 'output/REddyProc/raw'
+dg.MAIN_IMG_DIR = 'output/REddyProc'
+
+
 # Full list of outputs (not all are included in OUTPUT_ORDER):
 # 'DC_H_f', 'DC_LE_f', 'DC_NEE_uStar_f', 'DC_Rg_f', 'DC_rH_f', 'DC_Tair_f', 'DC_VPD_f',
 # 'DSum_Rg_f', 'DSum_rH_f', 'DSum_Tair_f', 'DSum_VPD_f', 'DSumU_H_f', 'DSumU_LE_f', 'DSumU_NEE_uStar_f',
@@ -7,16 +12,9 @@
 # 'FP_NEE_uStar_f', 'FP_Reco_DT_uStar', 'FP_Reco_uStar',
 # 'FP_Rg', 'FP_Rg_f', 'FP_rH', 'FP_rH_f', 'FP_Tair', 'FP_Tair_f', 'FP_VPD', 'FP_VPD_f'
 
-CROP_POSTFIX = '_crop'
-CROP = ['FP_NEE', 'FP_NEE_uStar_f',
-        'FP_LE', 'FP_LE_f',
-        'FP_H', 'FP_H_f',]
-
-REMOVED_LEGEND_POSTFIX = '_bare'
-REMOVE_LEGENDS = ['FP_NEE_crop',
-                  'FP_LE_crop',
-                  'FP_H_crop']
-
+dg.extract_heatmap_legends(img_tags=['FP_NEE', 'FP_NEE_uStar_f', 'FP_LE', 'FP_LE_f', 'FP_H', 'FP_H_f'],
+                           tags_omit_legend=['FP_NEE', 'FP_LE', 'FP_H'],
+                           legend_fname_postfix='_legend')
 
 # just for the record: unicode in code is mediocre practice
 OUTPUT_ORDER = (
