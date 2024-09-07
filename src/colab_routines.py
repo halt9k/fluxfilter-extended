f6884a98c8b22e3a8a261100eceb89c3bb90abfa
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-07 06:20:39 +0300

    wip: main notebook is real data

--- src/colab_routines.py
+++ src/colab_routines.py
@@ -1,5 +1,30 @@
 # all functions here are to be mocked or cancelled during non-colab runs
+from enum import Enum, auto
+from decorator import decorator
 
+
+class RunMode(Enum):
+    LOCAL, COLAB = auto(), auto()
+
+try:
+    import google.colab
+except ImportError:
+    RUN_MODE = RunMode.LOCAL
+else:
+    RUN_MODE = RunMode.COLAB
+
+
+def colab_only(func):
+    def wrapper(*args, **kwargs):
+        if RUN_MODE == RunMode.COLAB:
+            return func(*args, **kwargs)
+        else:
+            print(f"Skipping {func.__name__}, not in Colab mode.")
+            return None
+    return wrapper
+
+
+@colab_only
 def workaround_stop_scroll():
     from google.colab.output import eval_js
     eval_js('google.colab.output.setIframeHeight("10000")')
