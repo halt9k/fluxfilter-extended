5ee844cef69cfa1b1de4222d07ad33263a625fc9
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-07 11:29:56 +0300

    fix: scroll fix improved

--- src/colab_routines.py
+++ src/colab_routines.py
@@ -38,17 +38,24 @@ def colab_only_cell():
 
 
 @colab_only
-def workaround_stop_scroll():
-    # fixes autoscroll to bottom, but keeps scrollbar
+def no_scroll():
+    from google.colab import output
+    output.no_vertical_scroll()
 
-    from google.colab.output import eval_js
-    eval_js('google.colab.output.setIframeHeight("10000")')
 
-    # from IPython.display import HTML, display, Javascript, display_javascript
-    # js1 = Javascript('''
-    # async function resize_output() {
-    #     google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
-    #     google.colab.output.getActiveOutputArea().scrollTo(2000, 0);
-    # }
-    # ''')
-    # get_ipython().events.register('post_run_cell', resize_output)
+""" examples:
+from google.colab.output import eval_js
+eval_js('''
+let sh = google.colab.output.getActiveOutputArea().scrollHeight;
+google.colab.output.setIframeHeight(sh);
+''')
+
+from IPython.display import HTML, display, Javascript, display_javascript
+js1 = Javascript('''
+async function resize_output() {
+    google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
+    google.colab.output.getActiveOutputArea().scrollTo(2000, 0);
+}
+''')
+get_ipython().events.register('post_run_cell', resize_output)
+"""
