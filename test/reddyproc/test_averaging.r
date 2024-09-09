98f01bd8bec19f3a0e9ff00bd0febc444fc9b18c
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-09 07:52:03 +0300

    fix: averaging verified

--- test/reddyproc/test_averaging.r
+++ test/reddyproc/test_averaging.r
@@ -7,7 +7,7 @@ gc()
 # clear RStudio output
 cat("\014")
 
-options(error = browser)
+# options(error = browser)
 options(max.print = 100)
 # interactive() ?
 # on_debug = function() {	cat("\014")	browser()}
@@ -21,25 +21,36 @@ cat("Working dir is set to: ", project_dir, '\n')
 debugSource('src/reddyproc/postprocess_calc_averages.r')
 
 
+ensure_correct_names <- function(nd, nm, ny){
+	stopifnot(!duplicated(nd), !duplicated(nm), !duplicated(ny))
+	stopifnot(setdiff(nd, 'DoY') == nm, setdiff(nm, 'Month') == ny)
+
+	# no with .1 in name
+	stopifnot(!any(contains(match = '.1', vars = c(nd, nm, ny))))
+}
+
+
 test_model_3_month <- function(){
 	df = read.csv('test/reddyproc/test_averaging/3_months_long.txt', quote = NULL,  row.names = NULL)
-	df$Reco = NA
+	stopifnot(!'Reco' %in% colnames(df))
 
 	# ensure order and years are processed separately
 	df[df$Year == 2022 & df$DoY == 354 & df$Hour > 10,]$Year = 2023
 	df[df$Year == 2022 & df$DoY == 354,]$LE_f = 17
 	df[df$Year == 2023 & df$DoY == 354,]$LE_f = 11
 
-	# TODO missing columns ?
+	# ensure missing columns won't break
+	df$NEE_f = NULL
 
 	# ensure average
-	row_mask = df$Year == 2022 & between(df$DoY, 325, 365)
-	df[row_mask,]$VPD_f = df[row_mask,]$DoY
+	stopifnot(df[df$Year == 2023 & between(df$DoY, 32, 59),]$Rg_f %>%
+			  	mean %>% between(46.6980, 46.6981))
 
 	# ensure NA calculated correctly
-	df[df$Year == 2023 & between(df$DoY, 1, 31),]$H_f = NA
+	df[df$Year == 2023 & between(df$DoY, 1, 31),]$H = NA
 
 	dfs <- calc_averages(df)
+	df <- NULL
 	dd <- df_daily <- dfs[[1]]
 	dm <- df_monthly <- dfs[[2]]
 	dy <- df_yearly <- dfs[[3]]
@@ -48,25 +59,21 @@ test_model_3_month <- function(){
 	stopifnot(dd[dd$Year == 2022 & dd$DoY == 354,]$LE_f == 17)
 	stopifnot(dd[dd$Year == 2023 & dd$DoY == 354,]$LE_f == 11)
 
-	# ensure NA calculated correctly
-	stopifnot(is.na.data.frame(dm[dm$Year == 2023 & dm$Month == 1,]$H_f))
-	stopifnot(dm[dm$Year == 2023 & dm$Month == 1,]$H_sqc == 0.0)
-	stopifnot(dm[dm$Year == 2022 & dm$Month == 12,]$H_sqc == 1.0)
-
 	# ensure order
 	stopifnot(dm$Year[1] == 2022 & dm$Year[length(dm$Year)] == 2023)
 
+	# ensure missing columns won't break
+	stopifnot(!'NEE_f' %in% colnames(dm), !'NEE' %in% colnames(dd))
+
 	#  ensure average
-	stopifnot(between(dm[dm$Year == 2022 & dm$Month == 12,]$VPD_f, 31*11, 31*12))
+	stopifnot(dm[dm$Year == 2023 & dm$Month == 2,]$Rg_f %>%
+			  	between(46.6980, 46.6981))
 
-	# no dupe cols or cols with .1 in name
-	names_dd = names(dd)
-	names_dm = names(dm)
-	names_dy = names(dy)
-	stopifnot(!any(c(duplicated(names_dd), duplicated(names_dm), duplicated(names_dy))))
-	stopifnot(!any(contains(match = '.1', vars = c(names_dd, names_dm, names_dy))))
+	# ensure NA calculated correctly
+	stopifnot(dm[dm$Year == 2023 & dm$Month == 1,]$H_sqc == 0.0)
 
-	cat('Test test_model_3_month ok \n')
+	ensure_correct_names(names(dd), names(dm), names(dy))
+	cat('Test test_model_3_month ok \n\n')
 }
 
 
@@ -78,16 +85,27 @@ test_real_year <- function(){
 	df[df$Year == 2023 & df$DoY == 354,]$LE_f = 17
 	df[df$Year == 2022 & df$DoY == 354,]$LE_f = 11
 
-	# TODO missing columns ?
-
 	# ensure average
 	row_mask = df$Year == 2023 & between(df$DoY, 325, 365)
 	df[row_mask,]$VPD_f = df[row_mask,]$DoY
 
+	# ensure no interference from similar columns
+	df$VPD_ff = df$DoY
+	df$VVPD_ff = df$DoY
+
 	# ensure NA calculated correctly
-	df[df$Year == 2023 & between(df$DoY, 1, 31),]$H_f = NA
+	nna_prc <- df[df$Year == 2022 & df$DoY == 354,]$LE %>%
+		{sum(!is.na(.)) / length(.)}
+	df[df$Year == 2023 & between(df$DoY, 1, 31),]$H_f <- NA
+	stopifnot(df[df$Year == 2022 & between(df$DoY, 1, 31),]$H %>% is.na)
+	df[df$Year == 2023 & between(df$DoY, 335, 365),]$H <- -5
+
+	# ensure single row val
+	df[df$Year == 2024,]$VPD = NA
+	df[df$Year == 2024,]$VPD_f = NA
 
 	dfs <- calc_averages(df)
+	df <- NULL
 	dd <- df_daily <- dfs[[1]]
 	dm <- df_monthly <- dfs[[2]]
 	dy <- df_yearly <- dfs[[3]]
@@ -97,24 +115,24 @@ test_real_year <- function(){
 	stopifnot(dd[dd$Year == 2022 & dd$DoY == 354,]$LE_f == 11)
 
 	# ensure NA calculated correctly
-	stopifnot(is.na.data.frame(dm[dm$Year == 2023 & dm$Month == 1,]$H_f))
+	stopifnot(dm[dm$Year == 2022 & dm$Month == 12,]$LE_sqc %>%
+			  	between(nna_prc - 0.1, nna_prc + 0.1))
+	stopifnot(dm[dm$Year == 2023 & dm$Month == 1,]$H_f %>% is.na)
 	stopifnot(dm[dm$Year == 2022 & dm$Month == 1,]$H_sqc == 0.0)
 	stopifnot(dm[dm$Year == 2023 & dm$Month == 12,]$H_sqc == 1.0)
 
 	# ensure order
-	stopifnot(dm$Year[1] == 2023 & dm$Year[length(dm$Year)] == 2022)
+	stopifnot(dm$Year[1] == 2022 & dm$Year[length(dm$Year)] == 2024)
 
 	#  ensure average
-	stopifnot(between(dm[dm$Year == 2022 & dm$Month == 12,]$VPD_f, 31*11, 31*12))
+	stopifnot(dm[dm$Year == 2023 & dm$Month == 12,]$VPD_f %>% between(31*11, 31*12))
 
-	# no dupe cols or cols with .1 in name
-	names_dd = names(dd)
-	names_dm = names(dm)
-	names_dy = names(dy)
-	stopifnot(!any(c(duplicated(names_dd), duplicated(names_dm), duplicated(names_dy))))
-	stopifnot(!any(contains(match = '.1', vars = c(names_dd, names_dm, names_dy))))
+	# ensure one val
+	stopifnot(dd[dd$Year == 2024,]$VPD_sqc == 0)
+	stopifnot(dy[dy$Year == 2024,]$VPD_f %>% is.na)
 
-	cat('Test test_real_year ok \n')
+	ensure_correct_names(names(dd), names(dm), names(dy))
+	cat('Test test_real_year ok \n\n')
 }
 
 
