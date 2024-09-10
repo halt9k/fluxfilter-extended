a4759a7db7d9cfc1e9a23286902b32a88db0ed7c
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-10 07:01:46 +0300

    fix: clear globals

--- src/ipynb_globals.py
+++ src/ipynb_globals.py
@@ -1,8 +1,8 @@
 # patch this file during tests to introduce missing globals
 
 # only site name like 'tv_fy4_22.14'
-ias_output_prefix: str
+ias_output_prefix: str = globals()['ias_output_prefix']
 
 # site name with years like 'tv_fy4_22.14_2024' or 'tv_fy4_22.14_23-25'
 # must be provided by REddyProc internal naming routines
-eddy_out_prefix: str
+eddy_out_prefix: str = ''
