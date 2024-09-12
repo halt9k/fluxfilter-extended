c136366bf3dd1ba8896c0cdd93e13000a8310f91
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-12 15:52:46 +0300

    test: globals preset

--- src/ipynb_globals.py
+++ src/ipynb_globals.py
@@ -3,8 +3,10 @@
 
 # only site name like 'tv_fy4_22.14'
 # do not change - ipynb global
+# ias_output_prefix: str = 'tv_fy4_22-14'
 ias_output_prefix: str
 
 # site name with years like 'tv_fy4_22.14_2024' or 'tv_fy4_22.14_23-25'
 # must be provided by REddyProc internal naming routines
+# eddy_out_prefix: str = 'tv_fy4_22-14_2023'
 eddy_out_prefix: str = ''
