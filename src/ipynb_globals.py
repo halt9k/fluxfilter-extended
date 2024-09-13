4d9f2a8fbed3ea2718b10a026c324e12aa9350b1
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-13 07:05:41 +0300

    test: fix for global vars mocking

--- src/ipynb_globals.py
+++ src/ipynb_globals.py
@@ -1,12 +1,12 @@
-# during tests to introduce missing globals add them to this file
-# and use from src.ipynb_globals import *
+# this file is for mocking missing globals during tests and local runs
+# expected us in every cell:from src.ipynb_globals import *
+# do not declare variables here, only describe
 
 # only site name like 'tv_fy4_22.14'
-# do not change - ipynb global
 # ias_output_prefix: str = 'tv_fy4_22-14'
 ias_output_prefix: str
 
 # site name with years like 'tv_fy4_22.14_2024' or 'tv_fy4_22.14_23-25'
 # must be provided by REddyProc internal naming routines
 # eddy_out_prefix: str = 'tv_fy4_22-14_2023'
-eddy_out_prefix: str = ''
+eddy_out_prefix: str
