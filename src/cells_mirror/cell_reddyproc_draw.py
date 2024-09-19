f31644657609cdcfd9348c0231fc3d9f2c3d01c7
halt9k <49090368+halt9k@users.noreply.github.com>
2024-09-19 11:59:44 +0300

    refactor

--- src/cells_mirror/cell_reddyproc_draw.py
+++ src/cells_mirror/cell_reddyproc_draw.py
@@ -2,7 +2,6 @@ from src.ipynb_globals import *
 from src.reddyproc.postprocess import create_archive
 from src.reddyproc.postprocess_graphs import EddyOutput, EddyImgTagHandler
 from src.colab_routines import add_download_button, no_scroll
-from src.ipynb_helpers import display_images
 
 is_ustar = eddyproc_options.is_to_apply_u_star_filtering
 usuffix = 'uStar_f' if is_ustar else 'f'
@@ -23,8 +22,7 @@ output_sequence = (
 
 tag_handler = EddyImgTagHandler(main_path='output/reddyproc',
                                 eddy_loc_prefix=eddy_out_prefix, img_ext='.png')
-eio = EddyOutput(output_sequence=output_sequence,
-                 tag_handler=tag_handler)
+eio = EddyOutput(output_sequence=output_sequence, tag_handler=tag_handler)
 eio.prepare_images()
 
 arc_path = create_archive(dir='output/reddyproc', arc_fname=eddy_out_prefix + '.zip',
@@ -32,7 +30,6 @@ arc_path = create_archive(dir='output/reddyproc', arc_fname=eddy_out_prefix + '.
 add_download_button(arc_path, 'Download all images')
 
 no_scroll()
-# TODO move to class?
-display_images(output_sequence, main_path='output/reddyproc', prefix=eddy_out_prefix)
+eio.display_images()
 
 tag_handler.display_tag_info(eio.extended_tags())
