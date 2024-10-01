f3610f66bb00c1fb313041043d828c31923d8b9c
halt9k <49090368+halt9k@users.noreply.github.com>
2024-10-01 10:03:00 +0300

    ﻿wip: allow missing images draft

--- src/helpers/py_helpers.py
+++ src/helpers/py_helpers.py
@@ -3,6 +3,10 @@ import contextlib
 
 @contextlib.contextmanager
 def catch(on_exception=None, err_types=Exception):
+    if not err_types:
+        yield
+        return
+
     try:
         yield
     except err_types as e:
--- src/reddyproc/postprocess_graphs.py
+++ src/reddyproc/postprocess_graphs.py
@@ -14,8 +14,7 @@ from PIL import Image
 import src.helpers.os_helpers  # noqa: F401
 from src.helpers.py_helpers import catch
 from src.ipynb_helpers import display_image_row
-from src.helpers.io_helpers import replace_fname_end
-from src.helpers.io_helpers import tags_to_files, tag_to_fname
+from src.helpers.io_helpers import tag_to_fname
 from src.helpers.image_tools import crop_monocolor_borders, Direction, grid_images, remove_strip, \
     ungrid_image
 
@@ -33,12 +32,21 @@ class EProcImgTagHandler:
         self.loc_prefix = eproc_options.out_info.fnames_prefix
         self.img_ext = img_ext
 
-    def tag_to_img_fname(self, tag):
-        return tag_to_fname(self.main_path, self.loc_prefix, tag, self.img_ext)
+    def tag_to_img_fname(self, tag, must_exist=True, warn_if_missing=True):
+        fname = tag_to_fname(self.main_path, self.loc_prefix, tag, self.img_ext, must_exist)
+        if warn_if_missing and not fname:
+            warn(f"Expected image is missing: {tag}")
 
-    def tags_to_img_fnames(self, tags, exclude_missing=True, warn_if_missing=True):
-        return tags_to_files(self.main_path, self.loc_prefix, tags, self.img_ext,
-                             exclude_missing, warn_if_missing)
+        return fname
+
+    def tags_to_img_fnames(self, tags, exclude_missing=True, must_exist=True, warn_if_missing=True):
+        res = {tag: self.tag_to_img_fname(tag, must_exist, warn_if_missing) for tag in tags}
+        missing_image_tags = [k for k, v in res.items() if not v]
+
+        if exclude_missing:
+            for t in missing_image_tags:
+                res.pop(t)
+        return res
 
     @staticmethod
     def test_tag(tag, prefix, suffix):
@@ -123,15 +131,14 @@ class EddyImgPostProcess:
         fixed = grid_images([c_title] + rows[1: row_count], 1)
         return fixed
 
-    def load_heatmap(self, raw_tag):
-        path = self.tag_handler.tag_to_img_fname(raw_tag)
+    def load_heatmap(self, path):
         img = Image.open(path)
 
         # in case of multiple years, they are columns
         maps, legends = self.ungrid_heatmap(img)
         cmap = crop_monocolor_borders(maps, sides='LR')
         clegend = crop_monocolor_borders(legends, sides='LR')
-        return path, cmap, clegend
+        return cmap, clegend
 
     def get_legend_scale(self, legend_col):
         legend = ungrid_image(legend_col, ny=self.total_years, flatten=True)[0]
@@ -142,34 +149,46 @@ class EddyImgPostProcess:
         raw_tag_f = self.tag_handler.remove_suffix(target_tag, PostProcSuffixes.HMAPS)
         raw_tag = self.tag_handler.raw_tag(raw_tag_f)
 
-        path, maps, legends = self.load_heatmap(raw_tag)
-        path_f, maps_f, legends_f = self.load_heatmap(raw_tag_f)
+        tps = self.tag_handler.tags_to_img_fnames([raw_tag, raw_tag_f], must_exist=True)
+        if len(tps) != 2:
+            return
+        else:
+            path, path_f = tps.values()
+
+        maps, legends = self.load_heatmap(path)
+        maps_f, legends_f = self.load_heatmap(path_f)
 
         if self.get_legend_scale(legends) != self.get_legend_scale(legends_f):
             print(f'\n\n Legends of heatmaps do not match, check original output of EProc for {target_tag} \n')
         merged = grid_images([maps, maps_f, legends_f], 3)
 
-        merged.save(self.tag_handler.tag_to_img_fname(target_tag))
+        merged.save(self.tag_handler.tag_to_img_fname(target_tag, must_exist=False))
         self.raw_img_duplicates += [path, path_f]
 
     def process_flux(self, target_tag):
         raw_tag = self.tag_handler.remove_suffix(target_tag, PostProcSuffixes.COMPACT)
-        path = self.tag_handler.tag_to_img_fname(raw_tag)
+
+        path = self.tag_handler.tag_to_img_fname(raw_tag, must_exist=True)
+        if not path:
+            return
         img = Image.open(path)
 
         fixed = self.compact_title_row(img, self.total_years + 1)
 
-        fixed.save(self.tag_handler.tag_to_img_fname(target_tag))
+        fixed.save(self.tag_handler.tag_to_img_fname(target_tag, must_exist=False))
         self.raw_img_duplicates += [path]
 
     def process_diurnal_cycle(self, target_tag):
         raw_tag = self.tag_handler.remove_suffix(target_tag, PostProcSuffixes.COMPACT)
-        path = self.tag_handler.tag_to_img_fname(raw_tag)
+
+        path = self.tag_handler.tag_to_img_fname(raw_tag, must_exist=True)
+        if not path:
+            return
         img = Image.open(path)
 
         fixed = self.compact_title_row(img, 5)
 
-        fixed.save(self.tag_handler.tag_to_img_fname(target_tag))
+        fixed.save(self.tag_handler.tag_to_img_fname(target_tag, must_exist=False))
         self.raw_img_duplicates += [path]
 
 
