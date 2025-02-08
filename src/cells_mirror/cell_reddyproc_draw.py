082367e29432b5c8473b7419af621cf646e4284a
halt9k <49090368+halt9k@users.noreply.github.com>
2025-02-08 06:39:18 +0300

    ci: extract dev branch

---
+++ src/cells_mirror/cell_reddyproc_draw.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from typing import List, Tuple, Union
+
+import src.ipynb_globals as ig
+from src.helpers.io_helpers import create_archive
+from src.reddyproc.postprocess_graphs import EProcOutputHandler, EProcImgTagHandler, EProcOutputGen
+from src.colab_routines import colab_add_download_button, colab_no_scroll
+
+tag_handler = EProcImgTagHandler(main_path='output/reddyproc', eproc_options=ig.eddyproc, img_ext='.png')
+eog = EProcOutputGen(tag_handler)
+
+output_sequence: Tuple[Union[List[str], str], ...] = (
+    "## Тепловые карты",
+    eog.hmap_compare_row('NEE_*'),
+    eog.hmap_compare_row('LE_f'),
+    eog.hmap_compare_row('H_f'),
+    "## Суточный ход",
+    eog.diurnal_cycle_row('NEE_*'),
+    eog.diurnal_cycle_row('LE_f'),
+    eog.diurnal_cycle_row('H_f'),
+    "## 30-минутные потоки",
+    eog.flux_compare_row('NEE_*'),
+    eog.flux_compare_row('LE_f'),
+    eog.flux_compare_row('H_f')
+)
+
+eio = EProcOutputHandler(output_sequence=output_sequence, tag_handler=tag_handler, out_info=ig.eddyproc.out_info)
+eio.prepare_images_safe()
+ig.arc_exclude_files = eio.img_proc.raw_img_duplicates
+
+eproc_arc_path = Path('output') / Path(ig.eddyproc.out_info.fnames_prefix + '.zip')
+create_archive(arc_path=eproc_arc_path, folders='output/reddyproc', top_folder='output/reddyproc',
+               include_fmasks=['*.png', '*.csv', '*.txt'], exclude_files=eio.img_proc.raw_img_duplicates)
+
+colab_add_download_button(eproc_arc_path, 'Download eddyproc outputs')
+
+colab_no_scroll()
+eio.display_images_safe()
+
+tag_handler.display_tag_info(eio.extended_tags())
---
+++ src/ipynb_helpers.py
@@ -0,0 +1,83 @@
+"""
+Unlike colab_routines.py, this file is expected to be used under local runs too.
+However, output may be auto replaced with text.
+"""
+import io
+from warnings import warn
+
+from IPython import get_ipython
+from IPython.display import display, HTML
+from PIL import Image
+from ipywidgets import widgets, HBox
+
+from src.helpers.image_tools import grid_images
+
+
+def display_image_row(paths):  
+    imgs = []
+    for path in paths:
+        try:
+            img = Image.open(path)
+        except:
+            continue
+        imgs += [img]
+    # TODO check paths exist where?
+    if len(imgs) < 1:
+            return
+
+    img_combined = grid_images(imgs, len(imgs))
+    # byte_arr = io.BytesIO()
+    # img_combined.save(byte_arr, format='PNG')
+    # from IPython.display import Image as IImage
+    # IImage(data=byte_arr.getvalue(), width=img_combined.width, unconfined=True)
+
+    # widgets.Image is not persistent on Colab load
+    # display(byte_arr) does not have horisonal scroll,
+    #     if vertical scroll is disabled on Colab
+    # Hbox(widgets.Output(display(byte_arr))) works fluently
+
+    out = widgets.Output(layout={'border': '1px solid black'})
+    with out:
+        display(img_combined)
+    hbox = HBox([out])
+    display(hbox)
+
+
+def ipython_only(func):
+    def wrapper(*args, **kwargs):
+        if get_ipython():
+            return func(*args, **kwargs)
+        else:
+            print(f"IPython env not detected. {func.__name__} is skipped by design.")
+            return None
+
+    return wrapper
+
+
+def css_enable_word_wrap(*args, **kwargs):
+    display(HTML('''
+    <style>
+        pre {
+            white-space: pre-wrap;
+        }
+    </style>
+    '''))
+
+
+def register_ipython_callback_once(event_name, cb):
+    ev = get_ipython().events
+    cb_unregs = [cb_old for cb_old in ev.callbacks[event_name] if cb_old.__name__ == cb.__name__]
+    if len(cb_unregs) == 1 and cb.__code__ == cb_unregs[0].__code__:
+        return
+
+    for cb_old in cb_unregs:
+        warn(f'Removing unexpected callback {cb_old}.')
+        ev.unregister(event_name, cb_old)
+
+    ev.register(event_name, cb)
+
+
+@ipython_only
+def enable_word_wrap():
+    register_ipython_callback_once('pre_run_cell', css_enable_word_wrap)
+    print("Word wrap in output is enabled.")
