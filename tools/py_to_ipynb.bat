07b1a3153da8f28571d4c7a17a82b95646476fb3
halt9k <49090368+halt9k@users.noreply.github.com>
2025-07-09 11:00:35 +0300

    wip: checkups

--- tools/py_to_ipynb.bat
+++ tools/py_to_ipynb.bat
@@ -1,3 +1,3 @@
 @echo off
-%CONDA_ENVS%\ipynb-3-11\python.exe -m jupytext --update --to notebook ..\FluxFilter.py
+%CONDA_ENVS%\ipynb-3-11-13\python.exe -m jupytext --update --to notebook ..\FluxFilter.py
 del ..\FluxFilter.py
