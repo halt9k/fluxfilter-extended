3147cf0943145a8dde9fc343515c990cf62669f0
halt9k <49090368+halt9k@users.noreply.github.com>
2025-09-17 16:49:51 +0300

    wip: todo update

--- src/ipynb_routines.py
+++ src/ipynb_routines.py
@@ -46,6 +46,9 @@ filters_min_max = {
 # TODO 1 QV add ias description in the intro (or idea was to cut more, not to expand?)
 # TODO 1 QOA add csf description in the intro
 
+# TODO 1 QOA set config above, run after or set interactively? 0.9.4 seems had it above (fundamental problem with execution sequence)
+
+# TODO 1 QV was 0.9.5 link used or 1.0.0?
 
 # DONE repair 0.9.2 - 0.9.5
 # DONE ensure no new lines anymore + spellcheck
