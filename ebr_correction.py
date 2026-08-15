04a85c04f495670a907d543189dadec19e668000
halt9k <49090368+halt9k@users.noreply.github.com>
2026-08-15 07:43:43 +0300

    wip

--- ebr_correction.py
+++ ebr_correction.py
@@ -1,21 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.2
-#   kernelspec:
-#     display_name: Python 3
-#     name: python3
-# ---
+
 import numpy as np
-# %% [markdown] id="HEead7faY22W"
-# # EBR correction
-
-# %% id="E4rv4ucOX8Yz"
-
 import pandas as pd
 
 from src.config.ff_config import QuantileIQRFilterConfig
@@ -27,7 +11,7 @@ data: pd.DataFrame = pd.read_pickle("test.pkl")
 # 'Rn_1_1_1' = 'NETRAD_1_1_1'
 # shf_1_1_1 = g_1_1_1
 
-data = data[0: 200]
+data = pd.concat([data[0: 200], data[-200: 0]])
 
 # assert в местное время 22:00 – 02:30 и 10:00-14:30 (ensure via solar radiation?)
 
@@ -49,15 +33,15 @@ Index(['timestamp_start', 'timestamp_end', 'dtime', 'alb_1_1_1',
 data['ebc_cf'] = (data['rn_1_1_1'] - data['shf_1_1_1']) / (data['h'] + data['le'])
 
 
-# removing spikes with IQR 1.5
+# ### removing spikes with IQR 1.5
 data['ebc_cf_f'] = data['ebc_cf'] 
 iqr_cfg = QuantileIQRFilterConfig(enabled=True, window_size_days=31, tgt_cols={'ebc_cf_f': 1.5})
-with debug_plot_changes(debug=True, df=data, cols=['ebc_cf_f'], extra_cols=None, title='iqr_filter'):
-    data, _ = quantile_iqr_filter(df_in=data, filters_db_in={'ebc_cf_f': []}, debug=False, cfg_quantile=iqr_cfg, df_copy=False)
-    data.loc[~data['ebc_cf_f_quantile_iqr_filter'].astype(bool), 'ebc_cf_f'] = np.nan
+# with debug_plot_changes(debug=True, df=data, cols=['ebc_cf_f'], extra_cols=None, title='iqr_filter'):
+data, _ = quantile_iqr_filter(df_in=data, filters_db_in={'ebc_cf_f': []}, debug=False, cfg_quantile=iqr_cfg, df_copy=False)
+data.loc[~data['ebc_cf_f_quantile_iqr_filter'].astype(bool), 'ebc_cf_f'] = np.nan
 
 
-# важны только в местное время 22:00–02:30 и 10:00-14:30
+# ### важны только в местное время 22:00–02:30 и 10:00-14:30
 idx_1 = data.index.indexer_between_time('22:00', '02:30')
 idx_2 = data.index.indexer_between_time('10:00', '14:30')
 idx = np.hstack((idx_1, idx_2))
@@ -67,19 +51,36 @@ time_mask[idx] = True
 data.loc[time_mask, 'ebc_cf_f'] = np.nan
 
 
+# ### preparing ebr_cf quantiles
 t_delta = pd.Timedelta(30, 'days')
 # t_delta = pd.Timedelta(3, 'hours')
-data['ebc_cf_f_count_in_15_days'] = data['ecf'].rolling(window=t_delta, center=True).count()
+data['ebc_cf_f_count_in_15_days'] = data['ebc_cf_f'].rolling(window=t_delta, center=True).count()
 
-data['ebc_cf_f_q25'].rolling(window=t_delta, center=True).quantile(0.25)
-data['ebc_cf_f_q50'].rolling(window=t_delta, center=True).quantile(0.50)
-data['ebc_cf_f_q75'].rolling(window=t_delta, center=True).quantile(0.75)
+data['ebc_cf_f_q25'] = data['ebc_cf_f'].rolling(window=t_delta, center=True).quantile(0.25)
+data['ebc_cf_f_q50'] = data['ebc_cf_f'].rolling(window=t_delta, center=True).quantile(0.50)
+data['ebc_cf_f_q75'] = data['ebc_cf_f'].rolling(window=t_delta, center=True).quantile(0.75)
+data['ebc_cf_f_mean'] = data['ebc_cf_f'].rolling(window=t_delta, center=True).mean()
 
-more_than_5_ecf = data['ebc_cf_f_count_in_15_days'] > 5
+gr_5_ecf = data['ebc_cf_f_count_in_15_days'] > 5
+data_gr5 = data[gr_5_ecf]
+between_1_5_ecf = data['ebc_cf_f_count_in_15_days'].isin(range(1, 5))
+data_b15 = data[between_1_5_ecf]
 
-data['h_corr'] = np.nan
-data['l_corr'] = np.nan
-data['h_corr'][more_than_5_ecf] = data['h'][more_than_5_ecf] * data['ebc_cf']
-data['l_corr'][more_than_5_ecf] = data['l'][more_than_5_ecf] * data['ebc_cf']
+
+# ### applying corrections where possible
+data['h_corr_25'] = data_gr5['h'] * data_gr5['ebc_cf_f_q25']
+data['h_corr_50'] = data_gr5['h'] * data_gr5['ebc_cf_f_q50']
+data['h_corr_75'] = data_gr5['h'] * data_gr5['ebc_cf_f_q75']
+data['l_corr_25'] = data_gr5['l'] * data_gr5['ebc_cf_f_q25']
+data['l_corr_50'] = data_gr5['l'] * data_gr5['ebc_cf_f_q50']
+data['l_corr_75'] = data_gr5['l'] * data_gr5['ebc_cf_f_q75']
+
+data['h_corr'] = data_b15['l'] * data_b15['ebc_cf_f_mean']
+data['l_corr'] = data_b15['l'] * data_b15['ebc_cf_f_mean']
+
+
+
+data['h_corr'][gr_5_ecf] = data['h'][gr_5_ecf] * data['ebc_cf']
+data['l_corr'][gr_5_ecf] = data['l'][gr_5_ecf] * data['ebc_cf']
 
 pass
